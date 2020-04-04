130ddb246af84f30133724d0a5c6ef2f250ffff0
Jaxon Terrell <terrellj8@students.rowan.edu>
2020-04-03 21:39:40 -0400

    Update accounts.js

--- routes/accounts.js
+++ routes/accounts.js
@@ -183,36 +183,25 @@ router.post(
 )
 
 router.get('/businesses/:business_id', (request, response) => {
-  // send back info for a particular business based on their unique business id
-  var combinedOutput;
-  let sql = 'select * from businesses where business_id = ?';
-  let sql1= 'SELECT path FROM business_discovery.business_images WHERE business_id = ?;'
-  let sql2 = 'select deal_id from business_discovery.business_deals where bus_deals_id = ?;'
-  db.query(sql, request.params.business_id, (error, results) => {
-    var queryJSON = results;
-    if(error) {
-      return console.error(error.message);
-    }
-    combinedOutput = queryJSON;
-  });  
-  db.query(sql1, request.params.business_id, (error, results) => {
-    var queryJSON1 = results;
-    console.log(results)
-    if(error) {
-      return console.error(error.message);
-    }
-    combinedOutput.push(queryJSON1);
-  });  
-  
-  db.query(sql2, request.params.business_id, (error, results) => {
-    var queryJSON2 = results;
-    console.log(results)
-    if(error) {
-      return console.error(error.message);
-    }
-    combinedOutput.push(queryJSON2);
-    response.json(combinedOutput);
-  });  
+ //send back info for a particular business based on their unique business id
+ let sqlArray = ['call selectBusiness(?);', 'call selectBusinessImages(?);', 'call selectBusinessHours(?);', 'call selectDeals(?);', 'call selectDealHours(?);'] 
+ var jsonArray = [];
+ for (let step = 0; step <= sqlArray.length-1; step++) {
+   db.query(sqlArray[step], request.params.business_id, (error, [[results]]) => {
+     if(error) {
+       return console.error(error.message);
+     }
+     //makes sure we only get a response once we've executed the last SQL proc 
+     if(step == (sqlArray.length-1)) {
+       jsonArray.push(results);
+       response.json(jsonArray);
+       console.log(jsonArray);
+     }
+     else {
+       jsonArray.push(results);
+     }
+   });  
+ };
 })
 
 router.get('/users/:user_id', (request, response) => {
