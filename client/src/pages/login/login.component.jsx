fdad1629964513e767eb5686449638e0e2afed7c
kozama38 <43677584+kozama38@users.noreply.github.com>
2020-04-19 13:51:57 -0400

    Implemented login component
    Created component for the login page that handles submitting a form, which contains a username and password field. Currently only logs the user object to the console.

--- client/src/pages/login/login.component.jsx
+++ client/src/pages/login/login.component.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './login.css';
+import axios from 'axios';
+
+class LoginInput extends Component {
+  state = {
+    username: '',
+    password: '',
+  };
+
+  handleUsernameChange = e => {
+    this.setState({ username: e.target.value });
+  }
+
+  handlePasswordChange = e => {
+    this.setState({ password: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    console.log({ user });
+  }
+
+  render() {
+    return (
+      <div className="center">
+
+      <form onSubmit={this.handleSubmit}>
+
+      <div class="container">
+
+        <label for="uname"><b>Username</b></label>
+        <input type="username" placeholder="Enter Username" username="uname" onChange={this.handleUsernameChange} required />
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" password="pwd" onChange={this.handlePasswordChange} required />
+        
+        <button type="submit">Login</button>
+
+      </div>
+
+    </form>
+
+    </div>
+    );
+  }
+}
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>LOGIN PAGE</h1>
-    </div>
+    <LoginInput />
   );
 };
 
