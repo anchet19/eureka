15ddf9b16a235fa3a5d108b7bda5be84005fefa8
Matthew Tam <tammat87@students.rowan.edu>
2020-05-01 20:31:55 -0400

    Small CSS Changes
    Small CSS changes as well as utilizing business_id as the key

--- client/src/pages/homepage/businessCard.component.jsx
+++ client/src/pages/homepage/businessCard.component.jsx
@@ -5,26 +5,28 @@ import { Card, CardContent, Typography } from '@material-ui/core';
 export default class BusinessCard extends Component {
     render() {
         return (
-            <Link to={`/details/` + this.props.business.business_id} style={{textDecoration: "none"}}>
-                <Card style={{ margin: 5}}>
-                    <CardContent style={{ padding: 10 }}>
-                        <span>
-                            <Typography variant="h6"    color="textPrimary"   component="h3"   style={{display: "inline", fontSize: "1rem"}}>
-                                {this.props.business.name}
+            <div className="BusinessCard" key={this.props.business.business_id}>
+                <Link to={`/details/` + this.props.business.business_id} style={{textDecoration: "none"}}>
+                    <Card style={{ margin: 5}}>
+                        <CardContent style={{ padding: 10 }}>
+                            <span>
+                                <Typography variant="h6"    color="textPrimary"   component="h3"   style={{display: "inline", fontSize: "1rem"}}>
+                                    {this.props.business.name}
+                                </Typography>
+                                <Typography variant="body1" color="textSecondary" component="span"    style={{float: "right"}}>
+                                    {this.props.key}
+                                </Typography>
+                            </span>
+                            <Typography variant="body1" color="textSecondary" component="p"    style={{fontSize: ".75rem"}}>
+                                {this.props.business.address}
                             </Typography>
-                            <Typography variant="body1" color="textSecondary" component="span"    style={{float: "right"}}>
-                                {this.props.index}
+                            <Typography variant="body1" color="textSecondary" component="p"    style={{fontSize: ".75rem"}}>
+                                {this.props.business.cuisine}
                             </Typography>
-                        </span>
-                        <Typography variant="body1" color="textSecondary" component="p"    style={{fontSize: ".75rem"}}>
-                            {this.props.business.address}
-                        </Typography>
-                        <Typography variant="body1" color="textSecondary" component="p"    style={{fontSize: ".75rem"}}>
-                            {this.props.business.cuisine}
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Link>
+                        </CardContent>
+                    </Card>
+                </Link>
+            </div>
         )
     }
 }
